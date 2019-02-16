0035e22a52e4e54556828fa463c68d18228a5acb
Othmane <mahfoud.othmane97@gmail.com>
2019-02-16 21:06:06 +0100

    create new todos

--- todo_backend/index.js
+++ todo_backend/index.js
@@ -1,7 +1,14 @@
-const express = require("express"),
-      port    = process.env.PORT || 3000,
-      app     = express();
+const express    = require("express"),
+      bodyParser = require("body-parser"),
+      port       = process.env.PORT || 3000,
+      app        = express();
 
+const todoRoutes = require("./routes/todos")
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use("/api/todos", todoRoutes);
 
 app.listen(port, function(){
   console.log("server listening on port: " + port);
---
+++ todo_backend/routes/todos.js
@@ -0,0 +1,25 @@
+const express = require("express"),
+      router  = express.Router(),
+      db      = require("../models");
+
+router.get("/", function(req, res){
+  db.Todo.find()
+  .then(function(todos){
+    res.json(todos);
+  })
+  .catch(function(err){
+    res.send(err);
+  });
+});
+
+router.post("/", function(req, res){
+  db.Todo.create(req.body)
+  .then(function(newTodo){
+    res.status(201).json(newTodo);
+  })
+  .catch(function(err){
+    res.send(err);
+  });
+})
+
+module.exports = router;
